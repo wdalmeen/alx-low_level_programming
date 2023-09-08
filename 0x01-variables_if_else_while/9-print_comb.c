863c72ae6ee97d9e40106ffdd1cb737f380d9246
wdalmeen <wdalmeen10@gmail.com>
2023-09-08 12:39:55 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,4 @@
-/*
- * File: 9-print_comb.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
-/**
- * main - Prints all possible combinations of single-digit numbers.
- *
- * Return: Always 0.
- */
 int main(void)
 {
 	int num;
